e86c67a5df442814aefaac53defe3b2914450850
toxicmoel <tatahnoellimnyuy@gmail.com>
2022-04-29 13:34:59 +0100

    changed url to repath

--- conceptu/urls.py
+++ conceptu/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+
 from django.contrib import admin
-from django.urls import include
+from django.urls import include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('products.urls')),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^', include('products.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
