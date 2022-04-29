adcb5bbd77f251566b4af1ab092c7870434bc579
toxicmoel <tatahnoellimnyuy@gmail.com>
2022-04-29 13:16:39 +0100

    added ip adress

--- conceptu/settings.py
+++ conceptu/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'i7v=ry2mk(gkab47rq+=n@4zjdkxldb*l7a4lr920e+t1yl@f2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.31.38.41']
 
 
 # Application definition
